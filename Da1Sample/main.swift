6658d18e57ef50920c97bc383b200aea4c6ca16e
moxDroid Labs Inc <pritam.world@gmail.com>
2019-06-06 21:02:35 -0400

    initial commit

--- Da1Sample/main.swift
+++ Da1Sample/main.swift
@@ -10,3 +10,16 @@ import Foundation
 
 print("Hello, World!")
 
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dog😼cow👺 \(你好)"
+
+let 👍🏻 = "Hello"
+
+print(👍🏻, π, 你好, 🐶🐮)
+
+print(Int.max, Int.min)
+
+
+
+
